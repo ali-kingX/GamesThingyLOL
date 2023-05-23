e17dc91d08704855343479e6e02fa95f6b9b91db
ali-kingX <93087009+ali-kingX@users.noreply.github.com>
2023-05-23 16:12:28 -0400

    Yes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+// index.js
+document.getElementById("slopeButton").addEventListener("click", function() {
+    var iframe = document.createElement("iframe");
+    iframe.src = "https://files.ufreegame.net/Slope-Ball/?2";
+    iframe.width = "100%";
+    iframe.height = "500px";
+    document.body.appendChild(iframe);
+    document.getElementById("slopeButton").style.display = "none";
+  });
+  
