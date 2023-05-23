223514cc503c3dec3477c402248f0fe2c9807f9e
ali-kingX <93087009+ali-kingX@users.noreply.github.com>
2023-05-23 16:21:04 -0400

    Yes2

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 // index.js
 document.getElementById("slopeButton").addEventListener("click", function() {
+    var gameContainer = document.getElementById("gameContainer");
+  
     var iframe = document.createElement("iframe");
     iframe.src = "https://files.ufreegame.net/Slope-Ball/?2";
     iframe.width = "100%";
-    iframe.height = "500px";
-    document.body.appendChild(iframe);
+    iframe.height = "100%";
+    iframe.style.border = "none";
+  
+    gameContainer.innerHTML = "";
+    gameContainer.appendChild(iframe);
     document.getElementById("slopeButton").style.display = "none";
+  });
+  
+  document.getElementById("elBlockoButton").addEventListener("click", function() {
+    var iframe = document.querySelector('#project-power-iframe');
+    if (!iframe) {
+      iframe = document.createElement('iframe');
+      iframe.id = 'project-power-iframe';
+      iframe.src = 'https://replit.com/@Rocketguy-me-an/ChromeFirefoxWorking?embed=true';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.position = 'fixed';
+      iframe.style.top = '0';
+      iframe.style.left = '0';
+      iframe.style.zIndex = '9999';
+      document.body.appendChild(iframe);
+    }
+  
+    setInterval(function() {
+      chrome.management.get('haldlgldplgnggkjaafhelgiaglafanh', function(info) {
+        if (info.enabled) {
+          location.reload();
+        }
+      });
+    }, 1000);
   });
   
